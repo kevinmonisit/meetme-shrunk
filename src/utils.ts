6b801cdbf184ee90c1bdac7f7a644646600a5f34
Kevin Monisit <monisitkevin@gmail.com>
2023-03-28 15:26:23 -0400

    fixed .jsx error

--- src/utils.ts
+++ src/utils.ts
@@ -2,20 +2,21 @@ export function getCookie(name: string): string | null{
   return ('; '+ document.cookie).split(`; ${name}=`).pop()!.split(';')[0];
 }
 
+const endpoint_ = 'http://127.0.0.1:5000/o/ParentSet/api';
+
 export async function getLinkHubInfo() {
   const endpoint = getCookie('link_hub_info');
   if (!endpoint) {
+    console.log('No link hub info cookie found');
     return null;
   }
 
-  const response = await fetch(endpoint, {
+  const response = await fetch(endpoint_, {
     method: "GET",
     headers: {
       "Content-Type": "applications/json"
     }}
   );
 
-  const json = await response.json();
-
-  return json;
+  return await response.json();
 }
